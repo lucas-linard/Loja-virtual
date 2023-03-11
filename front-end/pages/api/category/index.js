04df7c8ae45b4517ed1d8725cdeaecae8343739c
lucas <lucas.linard@outlook.com>
2023-03-10 22:36:10 -0300

    feature: api category delete

--- front-end/pages/api/category/index.js
+++ front-end/pages/api/category/index.js
@@ -3,14 +3,11 @@ export default function handler(req, res) {
     if (req.method === 'POST') {
       // Process a POST request
         const { nome } = req.body;
-        axios.post('http://localhost:8080/categorias', {nome})
-
-    } if(req.method === 'GET') {
-        axios.get('http://localhost:8080/categorias')
-        .then(function (response) {
-            res.status(200).json(response.data);
+        axios.post('http://localhost:8080/categorias', {nome}).then(function (r) {
+            res.status(200).json(r.data);
         })
-    }
+
+    } 
     if(req.method === 'PUT'){
         const { id, nome } = req.body;
         console.log(id, nome)
@@ -19,5 +16,17 @@ export default function handler(req, res) {
             res.status(200).json('OK!');
         })
         
+    }
+    if(req.method === 'DELETE'){
+        const { id } = req.query;
+
+        axios.delete(`http://localhost:8080/categorias/${id}`)
+        .then(function () {
+            res.status(200).json('Recurso excluído com sucesso!');
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.status(500).json('Erro ao excluir o recurso');
+        });    
     }
   }
